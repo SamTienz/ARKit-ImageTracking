17c58e0cd0aa9bbe584fe28b742bf3fdc3c632df
tien110807 <tien110807@gmail.com>
2018-09-10 18:09:52 +0800

    first commit

---
+++ ImageTracking/Classes/Controllers/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  ARMessage
+//
+//  Created by Tien on 2018/8/23.
+//  Copyright © 2018 Tien. All rights reserved.
+//
+
+import UIKit
+import ARKit
+class ViewController: UIViewController,ARSCNViewDelegate {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var statusLabel: UILabel!
+    lazy var showObjectAction: SCNAction = {
+        return .sequence([
+            .fadeIn(duration: 0.3),
+            .wait(duration: 0.3)
+            ])
+    }()
+    lazy var wolfNode: SCNNode = {
+        guard let scene = SCNScene(named: "Wolf.dae") else { return SCNNode() }
+        let nodes = SCNNode()
+        let childNodes = scene.rootNode.childNodes
+        for node in childNodes{
+            nodes.addChildNode(node)
+        }
+        nodes.position = SCNVector3(0, 0, 0)
+        nodes.scale = SCNVector3(0.3, 0.3, 0.3)
+        nodes.eulerAngles.x = -.pi
+        nodes.eulerAngles.z = -.pi/2
+        return nodes
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sceneView.delegate = self
+        //自動光源
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        resetTracking()
+    }
+    
+    
+    func resetTracking() {
+        guard let refImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else { return }
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.detectionImages = refImages
+        let options: ARSession.RunOptions = [.resetTracking, .removeExistingAnchors]
+        sceneView.session.run(configuration, options: options)
+        statusLabel.text = "Move camera to your piture"
+    }
+    @IBAction func resetBtnAction(_ sender: Any) {
+        resetTracking()
+    }
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let imageAnchor = anchor as? ARImageAnchor else { return }
+        let referenceImage = imageAnchor.referenceImage
+        let imageName = referenceImage.name ?? "Error"
+        
+        let objectNode = getObjectNode(detectedImageName: imageName)
+        objectNode.opacity = 0
+        objectNode.position.y = 0.2
+        objectNode.runAction(self.showObjectAction)
+        
+        node.addChildNode(objectNode)
+        
+        DispatchQueue.main.async{
+            self.statusLabel.text = "Image detected: \"\(imageName)\""
+        }
+    }
+    func getObjectNode(detectedImageName:String) -> SCNNode{
+        var node = SCNNode()
+        switch detectedImageName {
+        case "Valk":
+            node = wolfNode
+        default:
+            node = SCNNode()
+        }
+        return node
+    }
+}
+
